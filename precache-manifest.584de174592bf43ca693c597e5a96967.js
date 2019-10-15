a5714db1c91714f6e85dc48beed9cf7b4f4a9e73
Mel <melissayeejew@gmail.com>
2019-10-15 13:09:07 +0900

    Updates

---
+++ precache-manifest.584de174592bf43ca693c597e5a96967.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d69694af6d2c86593ae4490100313e00",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "984715bda0258a7fec89",
+    "url": "/todolist/static/js/2.ded35850.chunk.js"
+  },
+  {
+    "revision": "fa3e6205251d4f1598d5",
+    "url": "/todolist/static/js/main.8b9dbaca.chunk.js"
+  },
+  {
+    "revision": "4e1b8ed3c740f7eb5649",
+    "url": "/todolist/static/js/runtime-main.1395c765.js"
+  }
+]);
